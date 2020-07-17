fe63c006ba199df83fe595f9b2fb199d7a946ac8
pietro <pietro.pettinato@edu.unito.it>
2020-07-17 19:15:22 +0200

    tree building

--- prova.py
+++ prova.py
@@ -1,12 +1,15 @@
 import nltk
+from nltk import load_parser
 
-#sentence = """At eight o'clock on Thursday morning Arthur didn't feel very good."""
-sentence = """The cat sleeps"""
-tokens = nltk.word_tokenize(sentence)
-print(tokens)
-tagged = nltk.pos_tag(tokens)
-print(tagged[0:6])
+sentence = 'a dog bites Irene'.split()
+#sentence = 'Irene walks a dog'.split()
+#tokens = nltk.word_tokenize(sentence)
+print(sentence)
 
+#tagged = nltk.pos_tag(tokens)
+cp = load_parser('cfgrammar.fcfg', trace=0)
 
-nltk.data.load('data.fcfg')
+#nltk.data.load('cfgrammar.fcfg')
 
+for tree in cp.parse(sentence):
+    print(tree)
