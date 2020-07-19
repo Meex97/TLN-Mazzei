2a613038afc323003168bab4d38de6182f33417d
Massimo Donini <massimo.donini@edu.unito.it>
2020-07-19 20:23:33 +0200

    prove gramm

--- parsing.py
+++ parsing.py
@@ -1,9 +1,12 @@
 import nltk
 from nltk.corpus import treebank
 
-sentence = """At eight o'clock on Thursday morning Arthur didn't feel very good."""
+sentence = """Your big opportunity is flying out of here"""
 tokens = nltk.word_tokenize(sentence)
 tagged = nltk.pos_tag(tokens)
 entities = nltk.chunk.ne_chunk(tagged)
-t = treebank.parsed_sents('wsj_0001.mrg')[0]
-t.draw()
+#t = treebank.parsed_sents('wsj_0001.mrg')[0]
+
+print(tagged)
+entities.draw()
+
--- prova.py
+++ prova.py
@@ -1,13 +1,14 @@
 import nltk
 from nltk import load_parser
 
-sentence = 'a dog bites Irene'.split()
+
+sentence = 'Angus gives a bone to every dog'.split()
 #sentence = 'Irene walks a dog'.split()
 #tokens = nltk.word_tokenize(sentence)
 print(sentence)
 
 #tagged = nltk.pos_tag(tokens)
-cp = load_parser('cfgrammar.fcfg', trace=0)
+cp = load_parser('cfgrammar2.fcfg', trace=0)
 
 #nltk.data.load('cfgrammar.fcfg')
 
