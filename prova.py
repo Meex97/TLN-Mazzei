d0546d0586df432e8f45eedda9031abb343c1491
Massimo Donini <massimo.donini@edu.unito.it>
2020-07-29 06:18:26 +0200

    3 sentence

--- prova.py
+++ prova.py
@@ -3,10 +3,9 @@ from nltk import load_parser
 
 
 #sentence = 'Angus gives a bone to every dog'.split()
-#sentence = 'a dog bites Irene'.split()
 #sentence = 'You are imagining things'.split()
-sentence = 'There is a price on my head'.split()
-#tokens = nltk.word_tokenize(sentence)
+#sentence = 'There is a price on my head'.split()
+sentence = 'your big opportunity is flying out of here'.split()
 print(sentence)
 
 #tagged = nltk.pos_tag(tokens)
